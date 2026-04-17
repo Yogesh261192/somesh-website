00c9e0af2b8504beea0677f740f564a4cba3b3e7
Yogesh2611 <yogeshmamgain2611@gmail.com>
2026-04-17 21:55:48 +0530

    changes

--- components/virtual-consultation/checkout-page.tsx
+++ components/virtual-consultation/checkout-page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import { VirtualConsultationCheckoutContent } from "./checkout-content";
 
 export function VirtualConsultationCheckout(props: Parameters<typeof VirtualConsultationCheckoutContent>[0]) {
-  return <VirtualConsultationCheckoutContent {...props} />;
+  return (
+    <Suspense fallback={<div>Loading checkout...</div>}>
+      <VirtualConsultationCheckoutContent {...props} />
+    </Suspense>
+  );
 }
