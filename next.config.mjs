698e08abe1a55296f8d4d248beab4df32d1c5c40
SomeshMamgain <someshmamgain76@gmail.com>
2026-05-31 14:56:08 +0530

    changes done

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,21 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.delhiphysioathome.com',
+          },
+        ],
+        destination: 'https://delhiphysioathome.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 }
 
 export default nextConfig
